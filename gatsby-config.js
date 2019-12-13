9e4aa0b2b908ec19f407fe50e6f4221bb567d51e
Marcello Paiva <marcello.paiva@gmail.com>
2019-12-13 11:32:51 -0500

    Part 2 - Deploying to Netlify & Netlify-CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-material-ui`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
